9a905e94e3b48deff18030fec39672b50ad3bead
kevinguicherd@gmail.com <kevinguicherd@gmail.com>
2014-01-08 15:41:01 +0100

    Ajout de Zend_Navigation et du routage perso

---
+++ src/application/Core/controllers/ContactController.php
@@ -0,0 +1,17 @@
+<?php 
+
+/**
+ * 
+ * Controller de contact de l'application
+ * 
+ * @category    App1
+ * @package     Core
+ */
+class ContactController extends Zend_Controller_Action
+{
+	public function indexAction()
+	{
+		
+	}
+		
+}
---
+++ src/application/Core/controllers/ErrorController.php
@@ -0,0 +1,28 @@
+<?php 
+
+/**
+ * 
+ * Controller d'erreur de l'application
+ * 
+ * @category    App1
+ * @package     Core
+ */
+class ErrorController extends Zend_Controller_Action
+{
+	public function init()
+	{
+		$this->_helper->layout()->setLayout('error');
+	}
+	
+	function errorAction()
+	{
+		$errorHandler = $this->_getParam('error_handler');
+		$this->view->exception = $errorHandler->exception;
+		
+		$fc = Zend_Controller_Front::getInstance();
+		$log = $fc->getParam('bootstrap')->getResource('log');
+		
+		$log->crit($errorHandler->exception->getMessage());
+	}
+		
+}
